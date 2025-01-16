e7e916dbaea6db08da010d7cd9df18318d4ee023
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-01-16 13:51:27 +0600

    Update purchase return queries to improve data structure and logging

--- src/db/store/query/purchase_return.js
+++ src/db/store/query/purchase_return.js
@@ -196,9 +196,10 @@ export async function selectPurchaseReturnEntryDetailsByPurchaseReturnUuid(
 			fetchData('/store/purchase-return-entry/by'),
 		]);
 		console.log('purchase_return', purchase_return);
+		console.log('purchase_return_entry', purchase_return_entry);
 		const response = {
-			...purchase_return?.data?.data,
-			purchase_return_entry: purchase_return_entry?.data?.data || [],
+			...purchase_return?.data,
+			purchase_return_entry: purchase_return_entry?.data || [],
 		};
 
 		const toast = {
