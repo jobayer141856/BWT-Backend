bd761bfd1fff2badb382923ec7506431de397fc3
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-03-25 15:18:43 +0600

    Refactor product transfer triggers to improve warehouse quantity updates and ensure correct assignment based on warehouse names

--- src/db/trigger/product_after_product_transfer.sql
+++ src/db/trigger/product_after_product_transfer.sql
@@ -1,13 +1,19 @@
 --inserted into database
 CREATE OR REPLACE FUNCTION product_after_product_transfer_insert_function()
 RETURNS TRIGGER AS $$
+
+DECLARE 
+    warehouse_name TEXT;
 BEGIN
+   SELECT assigned INTO warehouse_name FROM store.warehouse WHERE uuid = NEW.warehouse_uuid;
    UPDATE
         store.product
     SET
-        warehouse_1 = warehouse_1 - CASE WHEN warehouse_1_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END,
-        warehouse_2 = warehouse_2 - CASE WHEN warehouse_2_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END,
-        warehouse_3 = warehouse_3 - CASE WHEN warehouse_3_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END
+        
+        warehouse_1 = CASE WHEN warehouse_name = 'warehouse_1' THEN warehouse_1 - NEW.quantity ELSE warehouse_1 END,
+        warehouse_2 = CASE WHEN warehouse_name = 'warehouse_2' THEN warehouse_2 - NEW.quantity ELSE warehouse_2 END,
+        warehouse_3 = CASE WHEN warehouse_name = 'warehouse_3' THEN warehouse_3 - NEW.quantity ELSE warehouse_3 END
+  
     WHERE
         uuid = NEW.product_uuid;
     RETURN NEW;
@@ -16,15 +22,41 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION product_after_product_transfer_update_function()
 RETURNS TRIGGER AS $$
+
+DECLARE 
+    old_warehouse_name TEXT;
+    new_warehouse_name TEXT;
 BEGIN
-    UPDATE
-        store.product
-    SET
-        warehouse_1 = warehouse_1 - CASE WHEN warehouse_1_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END + CASE WHEN warehouse_1_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END,
-        warehouse_2 = warehouse_2 - CASE WHEN warehouse_2_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END + CASE WHEN warehouse_2_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END,
-        warehouse_3 = warehouse_3 - CASE WHEN warehouse_3_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END + CASE WHEN warehouse_3_uuid = NEW.warehouse_uuid THEN NEW.quantity ELSE 0 END
-    WHERE
-        uuid = NEW.product_uuid;
+    SELECT assigned INTO old_warehouse_name FROM store.warehouse WHERE uuid = OLD.warehouse_uuid;
+    SELECT assigned INTO new_warehouse_name FROM store.warehouse WHERE uuid = NEW.warehouse_uuid;
+    IF old_warehouse_name <> new_warehouse_name THEN
+        UPDATE
+            store.product
+        SET
+            warehouse_1 = CASE WHEN old_warehouse_name = 'warehouse_1' THEN warehouse_1 + OLD.quantity ELSE warehouse_1 END,
+            warehouse_2 = CASE WHEN old_warehouse_name = 'warehouse_2' THEN warehouse_2 + OLD.quantity ELSE warehouse_2 END,
+            warehouse_3 = CASE WHEN old_warehouse_name = 'warehouse_3' THEN warehouse_3 + OLD.quantity ELSE warehouse_3 END
+        WHERE
+            uuid = OLD.product_uuid;
+        UPDATE
+            store.product
+        SET
+            warehouse_1 = CASE WHEN new_warehouse_name = 'warehouse_1' THEN warehouse_1 - NEW.quantity ELSE warehouse_1 END,
+            warehouse_2 = CASE WHEN new_warehouse_name = 'warehouse_2' THEN warehouse_2 - NEW.quantity ELSE warehouse_2 END,
+            warehouse_3 = CASE WHEN new_warehouse_name = 'warehouse_3' THEN warehouse_3 - NEW.quantity ELSE warehouse_3 END
+        WHERE
+            uuid = NEW.product_uuid;
+
+    ELSE
+        UPDATE
+            store.product
+        SET
+            warehouse_1 = CASE WHEN old_warehouse_name = 'warehouse_1' THEN warehouse_1 + OLD.quantity - NEW.quantity ELSE warehouse_1 END,
+            warehouse_2 = CASE WHEN old_warehouse_name = 'warehouse_2' THEN warehouse_2 + OLD.quantity - NEW.quantity ELSE warehouse_2 END,
+            warehouse_3 = CASE WHEN old_warehouse_name = 'warehouse_3' THEN warehouse_3 + OLD.quantity - NEW.quantity ELSE warehouse_3 END
+        WHERE
+            uuid = OLD.product_uuid;
+    END IF;
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
@@ -32,18 +64,24 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION product_after_product_transfer_delete_function()
 RETURNS TRIGGER AS $$
 
+DECLARE 
+    warehouse_name TEXT;
 BEGIN
+    SELECT assigned INTO warehouse_name FROM store.warehouse WHERE uuid = OLD.warehouse_uuid;
     UPDATE
         store.product
     SET
-        warehouse_1 = warehouse_1 + CASE WHEN warehouse_1_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END,
-        warehouse_2 = warehouse_2 + CASE WHEN warehouse_2_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END,
-        warehouse_3 = warehouse_3 + CASE WHEN warehouse_3_uuid = OLD.warehouse_uuid THEN OLD.quantity ELSE 0 END
+        warehouse_1 = CASE WHEN warehouse_name = 'warehouse_1' THEN warehouse_1 + OLD.quantity ELSE warehouse_1 END,
+        warehouse_2 = CASE WHEN warehouse_name = 'warehouse_2' THEN warehouse_2 + OLD.quantity ELSE warehouse_2 END,
+        warehouse_3 = CASE WHEN warehouse_name = 'warehouse_3' THEN warehouse_3 + OLD.quantity ELSE warehouse_3 END
     WHERE
         uuid = OLD.product_uuid;
     RETURN OLD;
 END;
 $$ LANGUAGE plpgsql;
+
+
+-- Trigger
 
 CREATE OR REPLACE TRIGGER product_after_product_transfer_insert
 AFTER INSERT ON store.product_transfer
@@ -59,3 +97,5 @@ CREATE OR REPLACE TRIGGER product_after_product_transfer_delete
 AFTER DELETE ON store.product_transfer
 FOR EACH ROW
 EXECUTE FUNCTION product_after_product_transfer_delete_function();
+
+
